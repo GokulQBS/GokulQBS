564453b8b74f49149a48f91163aa418a4fb2d6a1
gokulkrish002 <itsgokul2002@gmail.com>
2023-11-04 08:56:49 +0530

    mass infra project started

---
+++ msssInfra/include/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" /> -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" href="images/logonew.png" sizes="32x32" />
+  <link rel="icon" href="images/logonew.png" sizes="192x192" />
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <title>Motherstransport</title>
+</head>
+<body>
+  <!-- <aside class="side-contents">
+    <button class="btn btn-dark-blue close-btn" id="side-bar-closer">
+      <i class="fa-solid fa-x"> <span class="fs-0">close sidebar</span></i>
+    </button>
+    <ul>
+      <li class="h3"><a href="#">Links</a></li>
+      <li class="fade-up-links"><a href="#">Home</a></li>
+      <li class="fade-up-links"><a href="#">Services</a></li>
+      <li class="fade-up-links"><a href="#">Projects</a></li>
+      <li class="fade-up-links"><a href="#">Clients</a></li>
+      <li class="fade-up-links"><a href="#">About</a></li>
+      <li class="fade-up-links"><a href="#">Contact</a></li>
+      <li class="fade-up-links"><a href="#">Gallery</a></li>
+    </ul>
+  </aside> -->
+  <main>
+
+   <nav class="navbar navbar-expand-lg navbar-light bg-light mx-3">
+    <a class="logo" href="#"><img src="lorem" alt="" class="object-cover"></a>
+    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+        aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+   </nav>
+
---
+++ mtem/index.php
@@ -0,0 +1,304 @@
+<?php include 'include/header.php' ?>
+
+
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="3000">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item position-relative active">
+          <img src="images/sliders/slider-img-1.png" class="d-block w-100 image-filter object-cover" alt="">
+
+          <div class="carousel-caption">
+            <div class="text-left">
+
+              <p class="carousel-title mt-3">Excavating Excellence<br>Every Time</p>
+              <p class="carousel-para h6">Your Trusted Partner for Precision Earthworks, Excavation, and Material
+                Supply.
+              </p>
+
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item position-relative ">
+          <img src="images/sliders/slider-img-2.png" class="d-block w-100 image-filter object-cover" alt="">
+
+          <div class="carousel-caption">
+            <div class="text-left">
+
+              <p class="carousel-title mt-3">Shaping Landscapes<br>Building Dreams</p>
+              <p class="carousel-para h6">Bringing Your Projects to Life, Safely and Sustainably. </p>
+
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item position-relative">
+          <img src="images/sliders/slider-img-3.png" class="d-block w-100 image-filter object-cover" alt="">
+
+          <div class="carousel-caption">
+            <div class="text-left">
+
+              <p class="carousel-title mt-3">Quality, Precision,<br>and Commitment</p>
+              <p class="carousel-para h6">We provide Good quality service, at affordable price.</p>
+
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item position-relative">
+          <img src="images/sliders/slider-img-5.png" class="d-block w-100 image-filter object-cover" alt="">
+
+          <div class="carousel-caption">
+            <div class="text-left">
+
+              <p class="carousel-title mt-3">27 years of <br>Experience</p>
+              <p class="carousel-para h6">we are the The Earthwork Experts you need.</p>
+
+            </div>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+
+
+    <div class="mx-0 mx-lg-5">
+      <div class="row my-5 align-items-center">
+        <div class="col-lg-6 col-md-12 col-sm-12">
+          <div class="px-4">
+            <div class="border-left-thick pl-3">
+              <p class="h5 mb-3 text-mtem" data-aos="fade-in" data-aos-duration="1000" data-aos-once="true">About us</p>
+              <p class="h2" data-aos="fade-left" data-aos-duration="2000" data-aos-once="true">In the Beginning</p>
+            </div>
+            <div class="pl-3 mt-5">
+              <p class="text-dark paragraph" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">In the late 1990s, the story of <span class="font-weight-bold">Mother
+                  Transport</span>
+                began with a passionate and hardworking individual, Mr. R.
+                KUPPUSAMY. From the humble role of a laborer, he quickly rose to become a labor contractor, and by 2001,
+                he
+                was handling the supply of bulk materials with just a single lorry. A visionary in the making, he
+                expanded
+                his
+                endeavors, diving into both labor contracting and material supply, laying the foundation for Mother
+                Transport.
+              </p>
+            </div>
+            <a href="#" class="btn btn-mtem mt-4 ml-4">Learn More</a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 d-none d-lg-block">
+          <div class="d-flex align-items-center justify-content-center">
+            <div class="beginning-img" data-aos="fade-left" data-aos-duration="2000" data-aos-once="true">
+              <img src="images/homepage/about_img.png" class="object-cover" alt="test-img">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="row py-5 mx-1 mx-md-2">
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="why-chooling-img p-3 mt-3" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
+          <img src="images/homepage/experience_img.png" class="object-contain" alt="">
+        </div>
+        <div class="total_experience" data-aos="fade-up" data-aos-duration="3000" data-aos-once="true">
+          <p class="experience_year">27</p>
+          <p class="experience_text">Years of <br> Experience</p>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="mb-3">
+          <p class="h5 mb-3 text-mtem">Expertise</p>
+          <p class="h2 font-weight-bold" data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">Why You Should</p>
+          <p class="h2 font-weight-bold" data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">Choose Us</p>
+        </div>
+        <div>
+          <p class="paragraph" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">We offer a diverse range of services to cater to your needs, including Foundation Works, Road Work,
+            Material
+            Supply, and Demolition Work. With our experienced team and top-notch equipment, your projects are in safe
+            hands.</p>
+          <div class="row mt-3">
+            <div class="col-lg-5 col-md-6 col-sm-12" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
+              <div class="p-3 bg-light text-center mt-3 hover-box-shadow">
+                <p class="h1 text-mtem">33+</p>
+                <p>Projects Done</p>
+              </div>
+            </div>
+            <div class="col-lg-5 col-md-6 col-sm-12" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
+              <div class="p-3 bg-light text-center mt-3 hover-box-shadow">
+                <p class="h1 text-mtem">32+</p>
+                <p>Happy Customers</p>
+              </div>
+            </div>
+            <div class="col-lg-5 col-md-6 col-sm-12" data-aos="fade-up" data-aos-duration="3000" data-aos-once="true">
+              <div class="p-3 bg-light text-center mt-3 hover-box-shadow">
+                <p class="h1 text-mtem">56+</p>
+                <p>Equipment Fleet</p>
+              </div>
+            </div>
+            <div class="col-lg-5 col-md-6 col-sm-12" data-aos="fade-up" data-aos-duration="3000" data-aos-once="true">
+              <div class="p-3 bg-light text-center mt-3 hover-box-shadow">
+                <p class="h1 text-mtem">23</p>
+                <p>Years of experience</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+    <div class="container my-3 py-3">
+      <div class="">
+        <p class="h5 text-mtem">Our Service</p>
+        <p class="h2 font-weight-bold " data-aos="fade-right" data-aos-duration="2000" data-aos-once="true"><span>What
+            are the
+            Services</span> <br> We offer</p>
+      </div>
+      <div class="row">
+        <div class="col-lg-12 mb-5">
+          <p class="mt-3 mt-md-0 text-justify paragraph" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">At Mothers Transport,
+            we
+            offer a comprehensive suite of construction services to bring your vision to life. From sturdy foundations
+            to
+            expert construction, reliable material supply, efficient demolitions, and road projects, we've got it all
+            covered.</p>
+          <p class="mt-3 mt-md-0 text-justify paragraph" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">we don't just provide services; we forge partnerships. We are your trusted partner in
+            progress, dedicated to delivering excellence in every service we offer. Whether you're planning a new
+            project or seeking support for your transportation and infrastructure needs, we are here to elevate your
+            endeavors.</p>
+        </div>
+
+
+        <div class="col-lg-6 col-md-6 col-sm-12 mt-5 mt-md-0" data-aos="fade-up" data-aos-duration="2000"
+          data-aos-once="true">
+          <div class="position-relative">
+            <div class="services-img">
+              <img src="images/about_img.png" class="object-cover br-rounded" alt="">
+            </div>
+            <i class="services-img2"></i>
+            <div class="services-content d-flex align-items-center justify-content-center">
+              <div class="text-center">
+                <div class="d-flex justify-content-center">
+                  <div class="services-thumbnail">
+                    <img src="images/homepage/excavation.png" class="object-contain" alt="">
+                  </div>
+                </div>
+                <p class="h5 special_text mt-3">Excavation Works</p>
+                <a href="#" class="special_link">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 mt-5 mt-md-0" data-aos="fade-up" data-aos-duration="2000"
+          data-aos-once="true">
+          <div class="position-relative"></div>
+          <div class="services-img">
+            <img src="images/raw-material.png" class="object-cover br-rounded" alt="">
+          </div>
+          <i class="services-img2"></i>
+          <div class="services-content d-flex align-items-center justify-content-center">
+            <div class="text-center">
+              <div class="d-flex justify-content-center">
+                <div class="services-thumbnail">
+                  <img src="images/homepage/material_supply.png" class="object-contain" alt="">
+                </div>
+              </div>
+              <p class="h5 special_text mt-3">Material Supply</p>
+              <a href="#" class="special_link">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-5 ml-lg-5">
+        <div class="col-lg-6 col-md-6 col-sm-12 " data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
+          <div class="position-relative"></div>
+          <div class="services-img">
+            <img src="images/homepage/road_works.png" class="object-cover br-rounded" alt="">
+          </div>
+          <i class="services-img2"></i>
+          <div class="services-content d-flex align-items-center justify-content-center">
+            <div class="text-center">
+              <div class="d-flex justify-content-center">
+                <div class="services-thumbnail">
+                  <img src="images/homepage/buildings.png" class="object-contain" alt="">
+                </div>
+              </div>
+              <p class="h5 special_text mt-3">Road works</p>
+              <a href="#" class="special_link">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="parallax-window">
+      <div class="mx-0 mx-lg-5 my-3 py-3">
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="border-left-thick pl-3 mb-3">
+            <p class="h2 mb-3 text-white" data-aos="fade-in" data-aos-duration="2000" data-aos-once="true">What's Our
+              Beautiful
+              Clients</p>
+            <p class="h2 text-white" data-aos="fade-left" data-aos-duration="2000" data-aos-once="true">Says About us
+            </p>
+          </div>
+        </div>
+
+        <div class="client-review" data-aos="fade-up" data-aos-duration="3000" data-aos-once="true">
+          <div class="p-3">
+            <div class="client-review-card">
+              <i class="fa-solid fa-quote-left"></i>
+              <p>MTEM Construction exceeded my expectations! Their attention to detail and commitment to quality are
+                unmatched. I couldn't be happier with the results.</p>
+              <p class="my-3"><span class="client-name">kumar,</span> <span class="h5 text-secondary">Chennai</span>
+              </p>
+            </div>
+          </div>
+          <div class="p-3">
+            <div class="client-review-card">
+              <i class="fa-solid fa-quote-left"></i>
+              <p>I had a tight deadline for my project, and MTEM delivered on time without compromising quality. Their
+                professionalism and efficiency are truly commendable.</p>
+              <p class="mt-3"><span class="client-name">Gokul,</span> <span class="h5 text-secondary">Chennai</span></p>
+            </div>
+          </div>
+          <div class="p-3">
+            <div class="client-review-card">
+              <i class="fa-solid fa-quote-left"></i>
+              <p>MTEM Construction is my go-to for all construction needs. They consistently provide top-notch service,
+                and
+                their team is a pleasure to work with."</p>
+              <p class="mt-3"><span class="client-name">Vignesh,</span> <span class="h5 text-secondary">Chennai</span>
+              </p>
+            </div>
+          </div>
+          <div class="p-3">
+            <div class="client-review-card">
+              <i class="fa-solid fa-quote-left"></i>
+              <p>The MTEM team transformed my property with their expertise. The finished project is not only
+                structurally
+                sound but also visually stunning.</p>
+              <p class="mt-3"><span class="client-name">Vijay,</span> <span class="h5 text-secondary">Chennai</span></p>
+            </div>
+          </div>
+          <div class="p-3">
+            <div class="client-review-card">
+              <i class="fa-solid fa-quote-left"></i>
+              <p>I highly recommend MTEM Construction for their commitment to safety. They made sure every aspect of the
+                project adhered to the highest safety standards.</p>
+              <p class="mt-3"><span class="client-name">Suriya</span>, <span class="h5 text-secondary">Chennai</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   <?php include 'include/footer.php' ?>
